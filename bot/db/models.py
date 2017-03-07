810cd4edd00aa0d59a43ea27b1f4bc8a3d523ee3
vanya <vanya.s.vanya@gmail.com>
2017-03-07 14:25:35 +0100

    add new models

--- bot/db/models.py
+++ bot/db/models.py
@@ -17,6 +17,33 @@ class Cars(Base):
     update_dt = Column(DATE)
 
 
+class StagingCars(Base):
+    __tablename__ = 'staging_cars'
+    id = Column(TEXT, primary_key=True)
+    base_url = Column(TEXT)
+    url = Column(TEXT)
+    desc = Column(TEXT)
+    price = Column(TEXT)
+    gear = Column(TEXT)
+    year = Column(TEXT)
+    mileage = Column(TEXT)
+    update_dt = Column(DATE)
+
+
+class RemovedCars(Base):
+    __tablename__ = 'removed_cars'
+    id = Column(TEXT, primary_key=True)
+    base_url = Column(TEXT)
+    url = Column(TEXT)
+    desc = Column(TEXT)
+    price = Column(TEXT)
+    gear = Column(TEXT)
+    year = Column(TEXT)
+    mileage = Column(TEXT)
+    update_dt = Column(DATE)
+    deletion_dt = Column(DATE, primary_key=True)
+
+
 class Subscribers(Base):
     __tablename__ = 'subscribers'
     chat_id = Column(TEXT, primary_key=True)
