e0b7306922f1383ec9eb84954b6ffec7096541b6
vanya <vanya.s.vanya@gmail.com>
2017-03-19 11:26:57 +0200

    Run only finished job

--- bot/handlers/scraryhub.py
+++ bot/handlers/scraryhub.py
@@ -8,6 +8,8 @@ def upload_iterator():
     conn = Connection(get_config()['SCRAPYHUB']['token'])
     project = conn[int(get_config()['SCRAPYHUB']['PROJECT_ID'])]
     for job in project.jobs():
+        if job.info['state'] != 'finished':
+            continue
         if 'carinfo_ais'in loaded_scrappers and 'carinfo_planeta' in loaded_scrappers:
             raise StopIteration()
         for item in job.items():
