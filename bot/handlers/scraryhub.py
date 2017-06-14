f70a0ca958116ca8be7a1e8737d94388d312eda2
vanya <vanya.s.vanya@gmail.com>
2017-06-14 22:04:41 +0300

    Update iterator

--- bot/handlers/scraryhub.py
+++ bot/handlers/scraryhub.py
@@ -4,20 +4,28 @@ from bot.config import get_config
 
 scrappers = {'carinfo_hitavto', 'carinfo_ais', 'carinfo_planeta'}
 
+MAX_RUNS = 10
+
 
 def upload_iterator():
     loaded_scrappers = set()
     conn = Connection(get_config()['SCRAPYHUB']['token'])
     project = conn[int(get_config()['SCRAPYHUB']['PROJECT_ID'])]
+    runned = 0
     for job in project.jobs():
+        if runned > MAX_RUNS:
+            raise StopIteration()
         if job.info['state'] != 'finished':
             continue
-        if not loaded_scrappers.difference(scrappers):
+        if not loaded_scrappers.symmetric_difference(scrappers):
             raise StopIteration()
+        if job.info['spider'] in loaded_scrappers:
+            continue
         for item in job.items():
             item.pop("_type", None)
             yield item
         loaded_scrappers.add(job.info['spider'])
+        runned += 1
 
 
 def clean_scrapyhub_jobs():
